2edfdac8418f56db26bd3375945f2df0b8fce3f2
Monali Ghosh <monalighosh@users.noreply.github.com>
2018-03-07 00:20:43 +1100

    Updating products section

--- website/src/js/form-validation.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-const forms = [...document.querySelectorAll("form")];
-forms.forEach((form) => form.addEventListener("submit", validateForm));
--- website/src/js/modal.js
+++ website/src/js/modal.js
@@ -1,51 +1,52 @@
-"use strict";
-// Get privacy modal element
-const modalPrivacy = document.querySelector(".privacy-modal");
-const privacyBtn = document.querySelector("#privacy");
-const privacyCloseBtn = document.querySelector(".privacy-modal__close-btn");
+(function() {
+  "use strict";
+  // Get privacy modal element
+  const modalPrivacy = document.querySelector(".privacy-modal");
+  const privacyBtn = document.querySelector("#privacy");
+  const privacyCloseBtn = document.querySelector(".privacy-modal__close-btn");
 
-// Get dealer modal element
-const modalDealer = document.querySelector(".dealer-modal");
-const dealerBtn = document.querySelector("#dealer");
-const dealerCloseBtn = document.querySelector(".dealer-modal__close-btn");
+  // Get dealer modal element
+  const modalDealer = document.querySelector(".dealer-modal");
+  const dealerBtn = document.querySelector("#dealer");
+  const dealerCloseBtn = document.querySelector(".dealer-modal__close-btn");
 
-// Button click event listeners
-privacyBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  openModal(modalPrivacy);
-});
+  // Button click event listeners
+  privacyBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    openModal(modalPrivacy);
+  });
 
-dealerBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  openModal(modalDealer);
-});
+  dealerBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    openModal(modalDealer);
+  });
 
-// Close button event listeners
-privacyCloseBtn.addEventListener("click", closeModal);
-dealerCloseBtn.addEventListener("click", closeModal);
+  // Close button event listeners
+  privacyCloseBtn.addEventListener("click", closeModal);
+  dealerCloseBtn.addEventListener("click", closeModal);
 
-// Window click event listener to close modal when clicked outside
-window.addEventListener("click", (e) => {
-  if(e.target === modalPrivacy) {
-    closeModalOnOutsideClick(modalPrivacy);
-  } else if(e.target === modalDealer) {
-    closeModalOnOutsideClick(modalDealer);
-  }  
-});
+  // Window click event listener to close modal when clicked outside
+  window.addEventListener("click", (e) => {
+    if(e.target === modalPrivacy) {
+      closeModalOnOutsideClick(modalPrivacy);
+    } else if(e.target === modalDealer) {
+      closeModalOnOutsideClick(modalDealer);
+    }  
+  });
 
-// Function to open modal
-function openModal(modalToOpen) {
-  modalToOpen.style.display = "block";
-}
+  // Function to open modal
+  function openModal(modalToOpen) {
+    modalToOpen.style.display = "block";
+  }
 
-// Function to close modal
-function closeModal() {
-  let parentModal = this.parentElement.parentElement;
-  parentModal.style.display = "none";
-}
+  // Function to close modal
+  function closeModal() {
+    let parentModal = this.parentElement.parentElement;
+    parentModal.style.display = "none";
+  }
 
-// Function to close modal when clicked outside
-function closeModalOnOutsideClick(modalToClose) {
-  modalToClose.style.display = "none";
-}
-
+  // Function to close modal when clicked outside
+  function closeModalOnOutsideClick(modalToClose) {
+    modalToClose.style.display = "none";
+  }
+})();
