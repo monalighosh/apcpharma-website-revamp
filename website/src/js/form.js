db1fc94e65ac939513ac2341e8883f8877ac1938
Monali Ghosh <monalighosh@users.noreply.github.com>
2018-03-09 11:28:53 +1100

    Updating forms

--- website/src/js/form.js
+++ website/src/js/form.js
@@ -1,44 +1,57 @@
 "use strict";
+// Get forms and register submit event
 const forms = [...document.forms];
 forms.forEach((form) => form.addEventListener("submit", submitFormData));
 
+// Function to submit form data and display success or fail message
 function submitFormData(e) {
   e.preventDefault();
+  // Get form action attribute tobe used in AJAX request
   const formAction = e.target.getAttribute("action");
+  // Get form elements
   let formEles = [...e.target.elements];
+  // New object to store user's data
   let formData = {};
+  // Add form element's name attribute and value to newly created object
   formEles.forEach((ele) => {
     if(ele.tagName !== "BUTTON") {
       let attr = ele.getAttribute("name");
       formData[attr] = ele.value;
     }
   });
-
+  // Create FormData object to store user's data tobe used in AJAX request
   let formDataToSend = new FormData();
+  for(let key in formData) {
+    formDataToSend.append(key, formData[key]);
+  }
+  // Create new div element for the form to display success or failure message
   const messageDiv =  document.createElement("div");
   let messageDivTxt = document.createTextNode("");
   messageDiv.appendChild(messageDivTxt);
   messageDiv.classList.add("form-submit-message");
-  for(let key in formData) {
-    formDataToSend.append(key, formData[key]);
-  }
 
+  // Create AJAX request to load the success or fail message without reloading page
   const xhr = new XMLHttpRequest();
   xhr.open("POST", formAction);
   xhr.onload = function() {
+    // If AJAX request status is success and response text from php form is "success" proceed further
     if(xhr.status === 200) {
       if(xhr.responseText == "success") {
+        // Append new div element to form which displays thank you message
         messageDiv.style.opacity = "1";
         messageDiv.innerHTML = `Thank you for getting in touch ${formData.name}! <br/>We will get back to you as soon as possible.`;
         e.target.appendChild(messageDiv);
+        // Reset form
         e.target.reset();
       } else {
+        // Append new div element to form which displays failure message
         messageDiv.style.opacity = "1";
         messageDiv.innerHTML = `Form submission failed.`;
         e.target.appendChild(messageDiv);
       }
     }
   };
+  // If AJAX request fails, log error message to console
   xhr.onerror = function() {
     console.error(`Request failed: ${xhr.responseText}`);
   };
--- website/src/js/main-nav.js
+++ website/src/js/main-nav.js
@@ -12,6 +12,7 @@
     const sectionId = e.target.getAttribute("href");
     const section = document.querySelector(sectionId);
     const sectionY = section.offsetTop;
+    // Check if the viewport size is greater or less than 920px and scroll to relevant section (resolving the header margin issue)
     window.innerWidth > 920 ?  window.scrollTo(0, sectionY-120) : window.scrollTo(0, sectionY-100);
     e.preventDefault();
   }
